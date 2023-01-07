777c8da2cdd286148602aed988a2b52c8500afeb
Lê Minh Đại <20521153@gm.uit.edu.vn>
2023-01-07 16:40:08 +0700

    build header #1

--- tiktok-ui/src/components/Layout/components/Header/index.js
+++ tiktok-ui/src/components/Layout/components/Header/index.js
@@ -1,5 +1,8 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
@@ -7,7 +10,23 @@ function Header() {
   // thẻ cha trong 1 component thì thêm class wrapper
   return (
     <header className={cx('wrapper')}>
-      <div className={cx('inner')}></div>
+      <div className={cx('inner')}>
+        <img src={images.logo} alt="logo tiktok" />
+
+        <div className={cx('search')}>
+          <input placeholder="Tìm kiếm tài khoản và video" spellCheck={false} />
+          <button className={cx('clear')}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+          <button className={cx('search-btn')}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+
+        <div className={cx('actions')}>actions</div>
+      </div>
     </header>
   );
 }
